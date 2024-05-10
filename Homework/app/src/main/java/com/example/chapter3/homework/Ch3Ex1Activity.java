8e91a23a353cb0d00e31c6541e42f758c3ca29ea
qiuyusen <1441830321@qq.com>
2024-05-10 21:32:19 +0800

    ex1

--- Homework/app/src/main/java/com/example/chapter3/homework/Ch3Ex1Activity.java
+++ Homework/app/src/main/java/com/example/chapter3/homework/Ch3Ex1Activity.java
@@ -1,10 +1,15 @@
 package com.example.chapter3.homework;
 
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.SeekBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 
 import com.airbnb.lottie.LottieAnimationView;
 
@@ -12,7 +17,7 @@ public class Ch3Ex1Activity extends AppCompatActivity {
     private LottieAnimationView animationView;
     private CheckBox loopCheckBox;
     private SeekBar seekBar;
-
+    private boolean isSeeking = false; // 标记是否正在手动拖动 SeekBar
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +26,7 @@ public class Ch3Ex1Activity extends AppCompatActivity {
         animationView = findViewById(R.id.animation_view);
         loopCheckBox = findViewById(R.id.loop_checkbox);
         seekBar = findViewById(R.id.seekbar);
+
 
         loopCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -44,14 +50,32 @@ public class Ch3Ex1Activity extends AppCompatActivity {
                 // 提示1：可以参考 https://airbnb.io/lottie/#/android?id=custom-animators
                 // 提示2：SeekBar 的文档可以把鼠标放在 OnProgressChanged 中间，并点击 F1 查看，
                 // 或者到官网查询 https://developer.android.google.cn/reference/android/widget/SeekBar.OnSeekBarChangeListener.html#onProgressChanged(android.widget.SeekBar,%20int,%20boolean
+                seekBar.setMax((int) animationView.getDuration());
+                if (fromUser) {
+                    // 用户拖动 SeekBar，更新动画的播放进度
+                    animationView.setProgress(progress / (float) animationView.getDuration());
+                }
             }
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
+                isSeeking = true;
+                animationView.pauseAnimation();
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
+                isSeeking = false;
+            }
+        });
+        animationView.addAnimatorUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                if (!isSeeking) {
+                    // 如果不是手动拖动 SeekBar，更新 SeekBar 的进度
+                    float progress = animation.getAnimatedFraction() * animationView.getDuration();
+                    seekBar.setProgress((int) progress);
+                }
             }
         });
     }
